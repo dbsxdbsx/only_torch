a9a056ec80a12d37462dff1b924fe19d7383305c
dbsx <602721790@qq.com>
2022-11-09 12:49:44 +0800

    测试更新

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,10 @@
+/// 测试用函数描述
+///
+/// # 示例
+///
+/// ```
+/// let dbsx - 5;
+/// ```
 pub fn add(left: usize, right: usize) -> usize {
     left + right
 }
@@ -8,7 +15,7 @@ mod tests {
 
     #[test]
     fn it_works() {
-        let result = add(2, 2);
+        let result = add(2, 1);
         assert_eq!(result, 4);
     }
 }
