6e4a0c21046835ee4598c8e0b36e36dbc7373879
dbsxdbsx <djw0214@gmail.com>
2023-08-18 17:33:39 +0800

    更新lib说明

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,7 @@
 //! # Only Torch
 //!
-//! `only_torch`项目旨在用纯rust将[pytorch](https://pytorch.org)这类基于梯度的机器学习算法和[NEAT](https://ieeexplore.ieee.org/document/6790655)这类网络突变（类似遗传算法）整合在一起，
+//! `only_torch`项目旨在用纯rust将[pytorch](https://pytorch.org)这类基于梯度的机器学习算法
+//! 和[NEAT](https://ieeexplore.ieee.org/document/6790655)这类网络突变（类似遗传算法）整合在一起，
 //! 打造一个相对来说轻便的跨平台（windows，linux，android...）快速推理AI框架。
 //!
 
