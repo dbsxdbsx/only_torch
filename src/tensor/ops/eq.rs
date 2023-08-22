95abb397709daea1f76fd6ca5d69e2bbd1417bed
dbsxdbsx <djw0214@gmail.com>
2023-08-22 15:23:53 +0800

    优化`==`算子的引用、所有权机制

--- src/tensor/ops/eq.rs
+++ src/tensor/ops/eq.rs
@@ -2,6 +2,35 @@ use std::cmp::PartialEq;
 
 use crate::tensor::Tensor;
 
+//↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓f32 ==（不）带引用的张量↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓
+impl PartialEq<Tensor> for f32 {
+    fn eq(&self, other: &Tensor) -> bool {
+        other.to_number().map_or(false, |x| x == *self)
+    }
+}
+
+impl<'a> PartialEq<&'a Tensor> for f32 {
+    fn eq(&self, other: &&'a Tensor) -> bool {
+        other.to_number().map_or(false, |x| x == *self)
+    }
+}
+//↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑f32 ==（不）带引用的张量↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑
+
+//↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓（不）带引用的张量 == f32↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓
+impl PartialEq<f32> for Tensor {
+    fn eq(&self, other: &f32) -> bool {
+        self.to_number().map_or(false, |x| x == *other)
+    }
+}
+
+impl<'a> PartialEq<f32> for &'a Tensor {
+    fn eq(&self, other: &f32) -> bool {
+        self.to_number().map_or(false, |x| x == *other)
+    }
+}
+//↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑（不）带引用的张量 == f32↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑
+
+//↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓（不）带引用的张量 == （不）带引用的张量↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓
 impl PartialEq for Tensor {
     fn eq(&self, other: &Self) -> bool {
         self.data == other.data
@@ -19,28 +48,4 @@ impl<'a> PartialEq<Tensor> for &'a Tensor {
         self.data == other.data
     }
 }
-
-// //
-impl PartialEq<f32> for Tensor {
-    fn eq(&self, other: &f32) -> bool {
-        self.to_number().map_or(false, |x| x == *other)
-    }
-}
-
-impl<'a> PartialEq<f32> for &'a Tensor {
-    fn eq(&self, other: &f32) -> bool {
-        self.to_number().map_or(false, |x| x == *other)
-    }
-}
-
-impl PartialEq<Tensor> for f32 {
-    fn eq(&self, other: &Tensor) -> bool {
-        other.to_number().map_or(false, |x| x == *self)
-    }
-}
-
-impl<'a> PartialEq<&'a Tensor> for f32 {
-    fn eq(&self, other: &&'a Tensor) -> bool {
-        other.to_number().map_or(false, |x| x == *self)
-    }
-}
+//↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑（不）带引用的张量 == （不）带引用的张量↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑
